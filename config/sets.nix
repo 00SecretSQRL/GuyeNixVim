d6f9e0353754909c576d1811349b96fdd11bc791
chris <cchiarulli@pm.me>
2024-10-06 16:46:34 -0400

    small config changes

--- config/sets.nix
+++ config/sets.nix
@@ -11,14 +11,15 @@
       relativenumber = true;
 
       # Set tabs to 2 spaces
-      tabstop = 2;
-      softtabstop = 2;
-      showtabline = 2;
+      tabstop = 4;
+      softtabstop = 4;
+      showtabline = 4;
       expandtab = true;
+      smarttab = true;
 
       # Enable auto indenting and set it to spaces
       smartindent = true;
-      shiftwidth = 2;
+      shiftwidth = 4;
 
       # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
       breakindent = true;
@@ -30,12 +31,13 @@
       # Enable text wrap
       wrap = true;
 
+      backspace="indent,eol,start"; # Allow backspacing over everything in insert mode
       # Better splitting
       splitbelow = true;
       splitright = true;
 
       # Enable mouse mode
-      mouse = "a"; # Mouse
+      mouse = ""; # Mouse
 
       # Enable ignorecase + smartcase for better searching
       ignorecase = true;
