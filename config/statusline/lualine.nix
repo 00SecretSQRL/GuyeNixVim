66ada0ae6b69ac18c632c4b9c650db1d83837745
chris <cchiarulli@pm.me>
2024-10-06 11:13:21 -0400

    Lualine changes in new version

--- config/statusline/lualine.nix
+++ config/statusline/lualine.nix
@@ -1,33 +1,37 @@
 {
   plugins.lualine = {
     enable = true;
-    alwaysDivideMiddle = true;
-    globalstatus = true;
-    ignoreFocus = ["neo-tree"];
-    extensions = ["fzf"];
-    theme = "dracula";
-    componentSeparators = {
-      left = "|";
-      right = "|";
-    };
-    sectionSeparators = {
-      left = "█"; # 
-      right = "█"; # 
-    };
-    sections = {
-      lualine_a = ["mode"];
-      lualine_b = [
-        {
-          name = "branch";
-          icon = "😈";
-        }
-        "diff"
-        "diagnostics"
-      ];
-      lualine_c = ["filename"];
-      lualine_x = ["filetype"];
-      lualine_y = ["progress"];
-      lualine_z = [''"👻" .. os.date("%R")''];
+    settings = {
+      options = {
+        alwaysDivideMiddle = true;
+        globalstatus = true;
+        ignoreFocus = ["neo-tree"];
+        extensions = ["fzf"];
+        theme = "dracula";
+        componentSeparators = {
+          left = "|";
+          right = "|";
+        };
+        sectionSeparators = {
+          left = "█"; # 
+          right = "█"; # 
+        };
+        sections = {
+          lualine_a = ["mode"];
+          lualine_b = [
+            {
+              name = "branch";
+              icon = "😈";
+            }
+            "diff"
+            "diagnostics"
+          ];
+          lualine_c = ["filename"];
+          lualine_x = ["filetype"];
+          lualine_y = ["progress"];
+          lualine_z = [''"👻" .. os.date("%R")''];
+        };
+      };
     };
   };
 }
