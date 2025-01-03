46d5e75c3fe75044bd23467efb68836a156e894c
chris <cchiarulli@pm.me>
2025-01-03 15:26:08 -0500

    test

--- config/ui/snacks.nix
+++ config/ui/snacks.nix
@@ -1,19 +1,43 @@
 {
-    plugins.snacks-nvim = {
-        enable = true;
-        settings= {
-            bigfile = { enabled = true; };
-            dashboard = { enabled = true; };
-            indent = { enabled = true; };
-            input = { enabled = true; };
-            notifier = { enabled = true; };
-            quickfile = { enabled = true; };
-            scratch = { enabled = true; };
-            scroll = { enabled = true; };
-            scope = { enabled = true; };
-            statuscolumn = { enabled = true; };
-            terminal = { enabled = true; };
-            words = { enabled = true; };
-        };
+  plugins.snacks = {
+    enable = true;
+    settings = {
+      bigfile = {
+        enabled = true;
+      };
+      dashboard = {
+        enabled = true;
+      };
+      indent = {
+        enabled = true;
+      };
+      input = {
+        enabled = true;
+      };
+      notifier = {
+        enabled = true;
+      };
+      quickfile = {
+        enabled = true;
+      };
+      scratch = {
+        enabled = true;
+      };
+      scroll = {
+        enabled = true;
+      };
+      scope = {
+        enabled = true;
+      };
+      statuscolumn = {
+        enabled = true;
+      };
+      terminal = {
+        enabled = true;
+      };
+      words = {
+        enabled = true;
+      };
     };
+  };
 }
