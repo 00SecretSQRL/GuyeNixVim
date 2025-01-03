95e4eaea314c16a49e797be724f38a062aecc2d5
chris <cchiarulli@pm.me>
2025-01-03 16:03:13 -0500

    moving to 24.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Guye made a flake for nixvim";
 
   inputs = {
-    nixpackages.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim.url = "github:nix-community/nixvim";
+    nixpackages.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixvim.url = "github:nix-community/nixvim/nixos-24.11";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
